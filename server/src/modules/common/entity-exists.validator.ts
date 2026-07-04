58ff0976da2c14b4da9dfe78f0169f36c793e993
张永乐 <zhangyongle@6cloudtech.com>
2026-07-04 12:19:21 +0800

    chore: add entity-exists validator stub

---
+++ server/src/modules/common/entity-exists.validator.ts
@@ -0,0 +1,7 @@
+import { ValidationOptions, registerDecorator } from 'class-validator';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class EntityExistsValidator {
+  // Placeholder for future entity existence validation
+}
